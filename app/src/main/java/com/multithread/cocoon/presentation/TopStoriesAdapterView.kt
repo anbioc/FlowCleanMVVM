c93d5090f3dcc5a9d4b2607fabe284861bcb2211
amir <olempico@gmail.com>
2020-11-14 19:50:20 +0330

    Loads view holder image.

--- app/src/main/java/com/multithread/cocoon/presentation/TopStoriesAdapterView.kt
+++ app/src/main/java/com/multithread/cocoon/presentation/TopStoriesAdapterView.kt
@@ -3,9 +3,11 @@ package com.multithread.cocoon.presentation
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.multithread.cocoon.data.model.dto.TopStoryDTO
+import com.multithread.cocoon.util.ImageLoader
 
 class TopStoriesAdapterView(
-    private val callback: (entity: TopStoryDTO.Result) -> Unit
+    private val callback: (entity: TopStoryDTO.Result) -> Unit,
+    private val imageLoader: ImageLoader
 ) : RecyclerView.Adapter<TopStoriesViewHolder>() {
 
     var itemList: List<TopStoryDTO.Result> = emptyList()
@@ -15,7 +17,7 @@ class TopStoriesAdapterView(
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TopStoriesViewHolder =
-        TopStoriesViewHolder.create(parent, callback)
+        TopStoriesViewHolder.create(parent, callback, imageLoader)
 
     override fun onBindViewHolder(holder: TopStoriesViewHolder, position: Int) =
         holder.bind(itemList[position])
--- app/src/main/java/com/multithread/cocoon/presentation/TopStoriesViewHolder.kt
+++ app/src/main/java/com/multithread/cocoon/presentation/TopStoriesViewHolder.kt
@@ -7,23 +7,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.multithread.cocoon.R
 import com.multithread.cocoon.data.model.dto.TopStoryDTO
 import com.multithread.cocoon.domain.model.TopStoryDomainEntity
+import com.multithread.cocoon.util.ImageLoader
 
 class TopStoriesViewHolder constructor(
     private val containerView: View,
     private val callback: (entity: TopStoryDTO.Result) -> Unit,
+    private val imageLoader: ImageLoader
 ) : RecyclerView.ViewHolder(containerView) {
 
     companion object {
-        fun create(parent: ViewGroup, callback: (entity: TopStoryDTO.Result) -> Unit) =
+        fun create(parent: ViewGroup,
+                   callback: (entity: TopStoryDTO.Result) -> Unit,
+                   imageLoader: ImageLoader) =
             TopStoriesViewHolder(
                 LayoutInflater.from(parent.context)
-                    .inflate(R.layout.item_story, parent, false), callback)
+                    .inflate(R.layout.item_story, parent, false), callback, imageLoader)
 
     }
 
     fun bind(story: TopStoryDTO.Result) {
         itemStoryTitle.text = story.title
         itemStoryDateTime.text = story.updatedDate
-
+        imageLoader.loadImage(
+            itemStoryImage,
+            0,
+            0,
+            story.multimedia[0].url
+        )
     }
 }
