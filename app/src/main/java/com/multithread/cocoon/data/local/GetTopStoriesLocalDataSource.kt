11ef70a8d1981ec34e74c94c610db0ec45a5e46d
amir <olempico@gmail.com>
2020-11-15 12:44:45 +0330

    Adds offlie-first capability to the repostiroy.

--- app/src/main/java/com/multithread/cocoon/data/local/GetTopStoriesLocalDataSource.kt
+++ app/src/main/java/com/multithread/cocoon/data/local/GetTopStoriesLocalDataSource.kt
@@ -2,13 +2,15 @@ package com.multithread.cocoon.data.local
 
 import com.multithread.cocoon.base.BaseDataSource
 import com.multithread.cocoon.base.LocalMapper
+import com.multithread.cocoon.base.ResultResponse
+import com.multithread.cocoon.base.wrapAroundSuccessResponse
 import com.multithread.cocoon.data.model.localEntity.TopStoryLocalEntity
 import com.multithread.cocoon.domain.model.TopStoryDomainEntity
 import javax.inject.Inject
 
 interface GetTopStoriesLocalDataSource : BaseDataSource {
     suspend fun saveTopStories(items: TopStoryDomainEntity)
-    suspend fun getTopStories(): TopStoryDomainEntity
+    suspend fun getTopStories(): ResultResponse<TopStoryDomainEntity>
 }
 
 class GetTopStoriesLocalDataSourceImpl @Inject constructor(
@@ -19,7 +21,7 @@ class GetTopStoriesLocalDataSourceImpl @Inject constructor(
     override suspend fun saveTopStories(items: TopStoryDomainEntity) =
             newsDao.insertOrUpdateAll(mapper.mapToLocal(items))
 
-    override suspend fun getTopStories(): TopStoryDomainEntity =
-            mapper.mapFromLocal(newsDao.getTopStories())
+    override suspend fun getTopStories(): ResultResponse<TopStoryDomainEntity> =
+            mapper.mapFromLocal(newsDao.getTopStories()).wrapAroundSuccessResponse()
 
 }
--- app/src/main/java/com/multithread/cocoon/domain/repository/TopStoriesRepository.kt
+++ app/src/main/java/com/multithread/cocoon/domain/repository/TopStoriesRepository.kt
@@ -3,24 +3,24 @@ package com.multithread.cocoon.domain.repository
 import com.multithread.cocoon.base.AnyParam
 import com.multithread.cocoon.base.ResultResponse
 import com.multithread.cocoon.base.repository.StrategyFlowRepository
+import com.multithread.cocoon.data.local.GetTopStoriesLocalDataSource
 import com.multithread.cocoon.data.remote.GetTopStoriesRemoteDataSource
 import com.multithread.cocoon.domain.model.TopStoryDomainEntity
 import com.multithread.cocoon.error.ErrorContainer
 import javax.inject.Inject
 
 class TopStoriesRepository @Inject constructor(
-    private val remoteTopStoriesDataSource: GetTopStoriesRemoteDataSource,
-    errorContainer: ErrorContainer
+        private val localTopStoriesDataSource: GetTopStoriesLocalDataSource,
+        private val remoteTopStoriesDataSource: GetTopStoriesRemoteDataSource,
+        errorContainer: ErrorContainer
 ) : StrategyFlowRepository<TopStoryDomainEntity, AnyParam>(errorContainer) {
 
-    override suspend fun getLocal(param: AnyParam): ResultResponse<TopStoryDomainEntity> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getLocal(param: AnyParam): ResultResponse<TopStoryDomainEntity> =
+            localTopStoriesDataSource.getTopStories()
 
     override suspend fun getRemote(param: AnyParam): ResultResponse<TopStoryDomainEntity> =
-        remoteTopStoriesDataSource.getTopStories()
+            remoteTopStoriesDataSource.getTopStories()
 
-    override suspend fun saveRemote(data: TopStoryDomainEntity) {
-        TODO("Not yet implemented")
-    }
+    override suspend fun saveRemote(data: TopStoryDomainEntity) =
+            localTopStoriesDataSource.saveTopStories(data)
 }
